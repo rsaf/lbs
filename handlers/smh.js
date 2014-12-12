6d0cfde9a1d32c08a5eac79ff66761d23b45d232
LBS.A.005 <lbsa005@LBSA005s-MacBook-Air.local>
2014-12-12 16:25:40 +0800

    added smm handlers

--- handlers/smh.js
+++ handlers/smh.js
@@ -3,8 +3,81 @@
  */
 
 
-module.exports = function(paramService, paramESB){
-    var serviceManagementRouter = paramService.Router();
+module.exports = function(paramService,  esbMessage){
+  var serviceManagementRouter = paramService.Router();
+  serviceManagementRouter.post('/newservice.json', function(paramRequest, paramResponse, paramNext){
+    var m = {
+      "ns":"smm",
+      "op": "createService",
+      "userid":paramRequest.user.id,//better not have the userid in the payload or it gets saved
+      "pl": {
+      //  organisationid:'',
+      //  groupName: String, //groupName
+      //  description: String,
+      //  memberCount: Number,
+      //  serviceCode: String,
+      //  serviceName:String,
+      //  serviceType:String,
+      //  serviceProvider:String,
+      //  price:Number,
+        briefOverview:'hello'
+      }
+    };
+    esbMessage(m)
+    .then(function(r) {
+      paramResponse.writeHead(200, {"Content-Type": "application/json"});
+      paramResponse.end(JSON.stringify(r));
+    })
+    .fail(function(r) {
+      paramResponse.writeHead(501, {"Content-Type": "application/json"});
+      if(r.er && r.er.ec && r.er.ec>1000){
+        r.er.em='Server poblem....';
+      }
+      paramResponse.end(JSON.stringify(r));
+    });
+  });
+  serviceManagementRouter.get('/myservices.json', function(paramRequest, paramResponse, paramNext){
+    var m = {
+      "ns":"smm",
+      "op": "servicesByCreator",
+      "pl": {
+        "userAccountID":paramRequest.user.id
+      }
+    };
+    esbMessage(m)
+    .then(function(r) {
+      paramResponse.writeHead(200, {"Content-Type": "application/json"});
+      paramResponse.end(JSON.stringify(r));
+    })
+    .fail(function(r) {
+      paramResponse.writeHead(501, {"Content-Type": "application/json"});
+      if(r.er && r.er.ec && r.er.ec>1000){
+        r.er.em='Server poblem....';
+      }
+      paramResponse.end(JSON.stringify(r));
+    });
+  });
+  serviceManagementRouter.get('/myservicepoints.json', function(paramRequest, paramResponse, paramNext){
+    var m = {
+      "ns":"smm",
+      "op": "servicePointsByCreator",
+      "pl": {
+        "userAccountID":paramRequest.user.id
+      }
+    };
+    esbMessage(m)
+    .then(function(r) {
+      paramResponse.writeHead(200, {"Content-Type": "application/json"});
+      paramResponse.end(JSON.stringify(r));
+    })
+    .fail(function(r) {
+      paramResponse.writeHead(501, {"Content-Type": "application/json"});
+      if(r.er && r.er.ec && r.er.ec>1000){
+        r.er.em='Server poblem....';
+      }
+      paramResponse.end(JSON.stringify(r));
+    });
+  });
 
-    return serviceManagementRouter;
+  return serviceManagementRouter;
 };
