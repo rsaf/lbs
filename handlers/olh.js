f62d10a6ab95bd4e373d50d7a4ff24ae5087e92f
LBS.A.005 <lbsa005@LBSA005s-MacBook-Air.local>
2014-12-12 16:47:55 +0800

    trying to merge

--- handlers/olh.js
+++ handlers/olh.js
@@ -1,87 +1,86 @@
-
 //All lanID specific business logics for the loging module are handle here
 /*
-var m = {
-"ns":"olm",
-"op": "readOperationsLog",
-"pl": {"userAccountID": "value1", "opType":"value2", "pageNumber":"value3", "pageSize":"value4"}
-};
+ var m = {
+ "ns":"olm",
+ "op": "readOperationsLog",
+ "pl": {"userAccountID": "value1", "opType":"value2", "pageNumber":"value3", "pageSize":"value4"}
+ };
 
-var r = {
-  "er":"value",
-   "pl": "value",
-   "mv": null
-}
-*/
+ var r = {
+ "er":"value",
+ "pl": "value",
+ "mv": null
+ }
+ */
 
 
-module.exports = function(paramService, paramESB)
+module.exports = function(paramService, esbMessage)
 {
-var operationsLogRouter = paramService.Router();
+  var operationsLogRouter = paramService.Router();
 
-operationsLogRouter.get('/all.json', function(paramRequest, paramResponse, paramNext){
+  operationsLogRouter.get('/all.json', function(paramRequest, paramResponse, paramNext){
 
-  //console.log('get all json called  ');
+    //console.log('get all json called  ');
 
-  var m = {
-  "ns":"olm",
-  "op": "readOperationsLog",
-  "pl": {"userAccountID":paramRequest.user.id, "opType":null, "pageNumber":1, "pageSize":10}
-  };
+    var m = {
+      "ns":"olm",
+      "op": "readOperationsLog",
+      "pl": {"userAccountID":paramRequest.user.id, "opType":null, "pageNumber":1, "pageSize":10}
+    };
 
-paramESB.message(m)
-    .then(function(r) {
-      paramResponse.writeHead(200, {"Content-Type": "application/json"});
-      paramResponse.end(JSON.stringify(r));
-    })
-    .fail(function(r) {
+    esbMessage(m)
+        .then(function(r) {
+          paramResponse.writeHead(200, {"Content-Type": "application/json"});
+          paramResponse.end(JSON.stringify(r));
+        })
+        .fail(function(r) {
 
-    });
+        });
 
-});
+  });
 
-operationsLogRouter.get('/business.json', function(paramRequest, paramResponse, paramNext){
-  //console.log('get business json called  ');
+  operationsLogRouter.get('/business.json', function(paramRequest, paramResponse, paramNext){
+    //console.log('get business json called  ');
 
-  var m = {
-  "ns":"olm",
-  "op": "readOperationsLog",
-  "pl": {"userAccountID":paramRequest.user.id, "opType":"业务操作", "pageNumber":1, "pageSize":10}
-  };
+    var m = {
+      "ns":"olm",
+      "op": "readOperationsLog",
+      "pl": {"userAccountID":paramRequest.user.id, "opType":"业务操作", "pageNumber":1, "pageSize":10}
+    };
 
-paramESB.message(m)
-    .then(function(r) {
-      //console.log(r.pl);
-      paramResponse.writeHead(200, {"Content-Type": "application/json"});
-      paramResponse.end(JSON.stringify(r));
-    })
-    .fail(function(r) {
-      console.log(r.er);
-    });
+    esbMessage(m)
+        .then(function(r) {
+          //console.log(r.pl);
+          paramResponse.writeHead(200, {"Content-Type": "application/json"});
+          paramResponse.end(JSON.stringify(r));
+        })
+        .fail(function(r) {
+          console.log(r.er);
+        });
 
-});
+  });
 
-operationsLogRouter.get('/access.json',function(paramRequest, paramResponse, paramNext){
-  //console.log('get access json called  ');
+  operationsLogRouter.get('/access.json',function(paramRequest, paramResponse, paramNext){
+    //console.log('get access json called  ');
 
-  var m = {
-  "ns":"olm",
-  "op": "readOperationsLog",
-  "pl": {"userAccountID":paramRequest.user.id, "opType":"授权操作", "pageNumber":1, "pageSize":10}
-  };
+    var m = {
+      "ns":"olm",
+      "op": "readOperationsLog",
+      "pl": {"userAccountID":paramRequest.user.id, "opType":"授权操作", "pageNumber":1, "pageSize":10}
+    };
 
-paramESB.message(m)
-    .then(function(r) {
-      //console.log(r.pl);
-      paramResponse.writeHead(200, {"Content-Type": "application/json"});
-      paramResponse.end(JSON.stringify(r));
-    })
-    .fail(function(r) {
-      console.log(r.er);
-    });
+    esbMessage(m)
+        .then(function(r) {
+          //console.log(r.pl);
+          paramResponse.writeHead(200, {"Content-Type": "application/json"});
+          paramResponse.end(JSON.stringify(r));
+        })
+        .fail(function(r) {
+          console.log(r.er);
+        });
 
 
-});
+  });
 
   return operationsLogRouter;
-}
+};
