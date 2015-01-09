43a2682caac5e0275e7df607826b3f0284cb26a3
LBS.A.005 <lbsa005@LBSA005s-MacBook-Air.local>
2015-01-09 15:24:35 +0800

    Handlers for fake endpoints.

--- handlers/pmh.js
+++ handlers/pmh.js
@@ -110,6 +110,438 @@ module.exports = function (paramPS, esbMessage) {
         var form = '<form action="/upload" enctype="multipart/form-data" method="post"><input multiple="multiple" name="upload" type="file" /><br><br><input type="submit" value="Upload" /></form>';
         res.end(form);
     });
+    
+    //fake endpoints
+    psRouter.get('/:type.json', function(paramRequest, paramResponse, paramNext){
+      if (paramRequest.params.type === 'idPhotoStandard'){
+          oHelpers.sendResponse(paramResponse,200,idPhotoStandard);
+      }
+      else if(paramRequest.params.type === 'idPhotosUsage'){
+          oHelpers.sendResponse(paramResponse,200,idPhotosUsage);
+      }
+      else if(paramRequest.params.type === 'folders'){
+          oHelpers.sendResponse(paramResponse,200,folders);
+      }
+    });
+    
+    
+    
     return psRouter;
 };
+
+var idPhotosUsage = {
+  "pl": [{
+      "code": "Y001",
+      "field1": "证件办理事务",
+      "field2": "蓝证近照；蓝证通办-白色背景；蓝证通办-蓝色背景  等"
+    }
+    , {
+      "code": "Y002",
+      "field1": "中小学教育与学生学业事务",
+      "field2": "蓝证近照；蓝证通办-白色背景；蓝证通办-蓝色背景  等"
+    }, {
+      "code": "Y003",
+      "field1": "高等教育与学生学业事务",
+      "field2": "蓝证近照；蓝证通办-白色背景；蓝证通办-蓝色背景  等"
+    }, {
+      "code": "Y004",
+      "field1": "考试报名事务",
+      "field2": "蓝证近照；蓝证通办-白色背景；蓝证通办-蓝色背景  等"
+    }, {
+      "code": "Y005",
+      "field1": "中国出入境事务",
+      "field2": "中国护照；港澳通行证；台湾通行证"
+    }, {
+      "code": "Y006",
+      "field1": "亚洲国家签证事务",
+      "field2": "日本签证；韩国签证；韩国签证；"
+    }, {
+      "code": "Y007",
+      "field1": "欧洲国家签证事务",
+      "field2": "德国签证；法国签证；意大利签证；"
+    }, {
+      "code": "Y008",
+      "field1": "北美洲国家签证事务",
+      "field2": "美国签证；加拿大签证；"
+    }, {
+      "code": "Y009",
+      "field1": "南美洲国家签证事务",
+      "field2": "巴西签证；墨西哥签证；阿根廷签证；哥伦比亚签证；"
+    }]
+};
+
+
+var idPhotoStandard = {
+  "pl": [{
+      "code": "Z001",
+      "field1": "蓝证照近照",
+      "field2": "390",
+      "field3": "567",
+      "field4": "JPG"
+    }, {
+      "code": "Z002",
+      "field1": "蓝证通办-白色背景",
+      "field2": "660",
+      "field3": "827",
+      "field4": "JPG"
+    }, {
+      "code": "Z003",
+      "field1": "蓝证通办-蓝色背景",
+      "field2": "390",
+      "field3": "567",
+      "field4": "JPG"
+    }, {
+      "code": "Z004",
+      "field1": "蓝证通办-红色背景",
+      "field2": "390",
+      "field3": "867",
+      "field4": "JPG"
+    }, {
+      "code": "Z005",
+      "field1": "小1寸-白色背景",
+      "field2": "260",
+      "field3": "378",
+      "field4": "JPG"
+    }, {
+      "code": "Z006",
+      "field1": "小1寸-白色背景",
+      "field2": "260",
+      "field3": "567",
+      "field4": "JPG"
+    }, {
+      "code": "Z007",
+      "field1": "小1寸-蓝色背景",
+      "field2": "260",
+      "field3": "378",
+      "field4": "JPG"
+    }, {
+      "code": "Z008",
+      "field1": "小1寸-红色背景",
+      "field2": "390",
+      "field3": "567",
+      "field4": "JPG"
+    }, {
+      "code": "Z009",
+      "field1": "小1寸-白色背景",
+      "field2": "390",
+      "field3": "567",
+      "field4": "JPG"
+    }]
+};
+
+var folders = {
+  "pl": [{
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 1"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 2"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 3"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 4"
+    }, {
+      "foldername": "蓝证通用证照"
+    }, {
+      "foldername": "护照证照"
+    }, {
+      "foldername": "亚洲签证证照"
+    }, {
+      "foldername": "小一寸-白底 5"
+    }]
+  ,"total":33
+};
+
 
--- handlers/sch.js
+++ handlers/sch.js
@@ -18,6 +18,15 @@ module.exports = function(paramService, esbMessage)
         else if(paramRequest.params.userType === 'acl'){
             oHelpers.sendResponse(paramResponse,200,acl);
         }
+        else if(paramRequest.params.userType === 'systeminterfaces'){
+            oHelpers.sendResponse(paramResponse,200,systeminterfaces);
+        }
+        else if(paramRequest.params.userType === 'thirdpartyinterfaces'){
+            oHelpers.sendResponse(paramResponse,200,thirdpartyinterfaces);
+        }
+        else if(paramRequest.params.userType === 'interfaceUsers'){
+            oHelpers.sendResponse(paramResponse,200,interfaceUsers);
+        }
   });
 
   return photosRouter;
@@ -100,7 +109,6 @@ var groups = {
     }]
 };
 
-
 var acl = {
   "pl": [{
       "imageurl": "/commons/images/passportPhoto_other.jpg",
@@ -135,3 +143,89 @@ var acl = {
       "field3": "正常"
     }]
   };
+  
+var systeminterfaces = {
+  "pl": [{
+      "field1": "身份证核验接口",
+      "field2": "正常",
+      "field3": true,
+      "field4": "2014/07/05 9:23",
+      "field5": "公安局"
+    }, {
+      "field1": "证照核验接口",
+      "field2": "正常",
+      "field3": true,
+      "field4": "2014/07/15 10：10",
+      "field5": "公安局"
+    }, {
+      "field1": "支付宝接口",
+      "field2": "正常",
+      "field3": true,
+      "field4": "2014/07/22 9:40",
+      "field5": "阿里巴巴"
+    }, {
+      "field1": "微信接口",
+      "field2": "不正常",
+      "field3": false,
+      "field4": "2014/08/05 12:23",
+      "field5": "腾讯"
+    }
+    , {
+      "field1": "短信接口",
+      "field2": "正常",
+      "field3": true,
+      "field4": "2014/09/07 11:30",
+      "field5": "中国移动"
+    }]
+};
+
+var thirdpartyinterfaces = {
+  "pl": [{
+      "field1": "蓝证证照接口",
+      "field2": "正常",
+      "field3": true,
+      "field4": "2014/07/05 9:23",
+      "field5": "蓝证平台"
+    }]
+};
+
+var interfaceUsers = {
+  "pl": [{
+      "imageurl": "/commons/images/passportPhoto_other.jpg",
+      "field1": "Andy",
+      "field2": "系统创建",
+      "field3": "2013/07/22",
+      "field4": "正常"
+    }, {
+      "imageurl": "/commons/images/passportPhoto_other.jpg",
+      "field1": "Candy",
+      "field2": "Andy",
+      "field3": "2013/05/10",
+      "field4": "正常"
+    }, {
+      "imageurl": "/commons/images/passportPhoto_other.jpg",
+      "field1": "Lsf",
+      "field2": "Andy",
+      "field3": "2013/05/20",
+      "field4": "正常"
+    }, {
+      "imageurl": "/commons/images/passportPhoto_other.jpg",
+      "field1": "雪中情",
+      "field2": "Andy",
+      "field3": "2013/06/13",
+      "field4": "正常"
+    }, {
+      "imageurl": "/commons/images/passportPhoto_other.jpg",
+      "field1": "漫天飞舞",
+      "field2": "雪中情",
+      "field3": "2013/05/05",
+      "field4": "正常"
+    }
+    , {
+      "imageurl": "/commons/images/passportPhoto_other.jpg",
+      "field1": "forever91",
+      "field2": "雪中情",
+      "field3": "2013/05/22",
+      "field4": "正常"
+    }]
+};
